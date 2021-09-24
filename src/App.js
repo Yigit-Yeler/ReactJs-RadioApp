dbeab9a116a121b6721002b51546062a55ac1ef3
YigitYeler <yeleryigit40@gmail.com>
2021-09-24 10:44:07 +0300

    completed project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react"
 import './App.css';
+import { DATA } from './Data'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
+  const [url, setUrl] = useState(null)
+
+  const changeUrl = (url) => {
+    setUrl(url)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <div className="header">
+        yStation
+      </div>
+      <div className="App">
+        {DATA.map((item) => (
+          <a onClick={() => changeUrl(item.url)}
+            className="radio-station"
+            style={{ cursor: "pointer" }}>
+            <img src={item.image}
+              style={{ width: 250, height: 250 }} />
+          </a>
+        ))}
+      </div>
+      <div className="player">
+        <AudioPlayer
+          autoPlay
+          showJumpControls={false}
+          src={url}
+          onPlay={e => console.log("onPlay")}
+        // other props here
+        />
+      </div>
+
     </div>
   );
 }
---
+++ src/Data.js
@@ -0,0 +1,10 @@
+export const DATA = [
+    { id: 1, url: "http://yayin.yayindakiler.com:4010/;stream.mp3", image: "https://www.radyocular.com/wp-content/uploads/2015/01/alemfm.jpeg" },
+    { id: 2, url: "http://turkmedya.radyotvonline.com/turkmedya/alemfm.stream/playlist.m3u8", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+    { id: 3, url: "http://37.247.98.7:80/;stream.mp3", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+    { id: 4, url: "http://46.20.7.125/listen.pls", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+    { id: 5, url: "http://provisioning.streamtheworld.com/pls/GONULAAC.pls", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+    { id: 6, url: "http://37.247.98.7/;stream.mp3", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+    { id: 7, url: "http://allergo.serverroom.us:8127", image: "https://mediacdns.karnaval.com/media/station_media/4/logos/meta_image.png?v=180520022614" },
+
+]
